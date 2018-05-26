ce7f7f680f938b285908298c8492abc74e5cfba9
Tiernan Martin <tiernan.martin@gmail.com>
2018-05-26 14:05:42 -0700

    add code for retrieving the wa-state-agency data

---
+++ wa-state-agencies/code/get-wa-state-agencies.R
@@ -0,0 +1,29 @@
+# SETUP ----
+
+library(rvest)
+library(tidyverse)
+
+# GET DATA ----
+
+agencies_url <- "https://access.wa.gov/agency.html"
+
+agencies_list <- read_html(agencies_url) %>% 
+  html_nodes(".agencyList") %>% 
+  html_text(trim = TRUE)
+
+agencies <- tibble(AGENCIES = agencies_list) %>% 
+  transmute(AGENCY_NAME_FULL = str_squish(str_replace_all(AGENCIES,"\r\n","")),
+            FOCUS = str_extract(AGENCY_NAME_FULL,".+(?=,)"),
+            TYPE = str_extract(AGENCY_NAME_FULL,"(?<=,[[:space:]]).+(?=\\s\\()"),
+            ACRONYM = str_extract(AGENCY_NAME_FULL,"(?<=\\().+(?=\\))")) %>%  
+  transmute(AGENCY_NAME = case_when(
+    is.na(FOCUS) ~AGENCY_NAME_FULL,
+    TRUE ~ str_c(TYPE, FOCUS,str_c("(",ACRONYM,")",sep=""),sep = " ")
+  ),
+  FOCUS,
+  TYPE,
+  ACRONYM)
+            
+# WRITE DATA ----
+
+write_csv(agencies, here::here("wa-state-agencies/data/wa-state-agencies.csv"))
