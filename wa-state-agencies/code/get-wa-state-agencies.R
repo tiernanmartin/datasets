79674c49db106130132ba896c6ef922da41e62e7
Tiernan Martin <tiernan.martin@gmail.com>
2018-05-26 17:34:37 -0700

    refine the Types variable

--- wa-state-agencies/code/get-wa-state-agencies.R
+++ wa-state-agencies/code/get-wa-state-agencies.R
@@ -1,6 +1,7 @@
 # SETUP ----
 
 library(rvest)
+library(tidytext)
 library(tidyverse)
 
 # GET DATA ----
@@ -12,11 +13,13 @@ agencies_list <- read_html(agencies_url) %>%
   html_text(trim = TRUE)
 
 agencies <- tibble(AGENCIES = agencies_list) %>% 
-  transmute(AGENCY_NAME_FULL = str_squish(str_replace_all(AGENCIES,"\r\n","")),
+  transmute(N = row_number(),
+            AGENCY_NAME_FULL = str_squish(str_replace_all(AGENCIES,"\r\n","")),
             FOCUS = str_extract(AGENCY_NAME_FULL,".+(?=,)"),
             TYPE = str_extract(AGENCY_NAME_FULL,"(?<=,[[:space:]]).+(?=\\s\\()"),
             ACRONYM = str_extract(AGENCY_NAME_FULL,"(?<=\\().+(?=\\))")) %>%  
-  transmute(AGENCY_NAME = case_when(
+  transmute(N,
+            AGENCY_NAME = case_when(
     is.na(FOCUS) ~AGENCY_NAME_FULL,
     TRUE ~ str_c(TYPE, FOCUS,str_c("(",ACRONYM,")",sep=""),sep = " ")
   ),
@@ -24,6 +27,38 @@ agencies <- tibble(AGENCIES = agencies_list) %>%
   TYPE,
   ACRONYM)
             
+types <- tibble::tribble(
+   ~TYPE_PATTERN, ~TYPE_CATEGORY,
+         "Board",        "board",
+        "Office",       "office",
+    "Commission",   "commission",
+    "Department",   "department",
+       "Council",      "council",
+     "Committee",    "committee",
+     "Authority",    "authority",
+       "Program",      "program",
+        "Agency",       "agency",
+         "Court",        "court",
+      "Division",     "division",
+     "Institute",    "institute",
+       "Library",      "library",
+        "Center",       "center",
+   "Legislature",  "legislature"
+  )
+
+agencies_types <- agencies %>% 
+  mutate(TOKEN = AGENCY_NAME) %>% 
+  unnest_tokens(TOKEN, TOKEN) %>% 
+  left_join(types, by = c(TOKEN = "TYPE_CATEGORY")) %>% 
+  group_by(N) %>% 
+  arrange(TYPE_PATTERN) %>% 
+  slice(1) %>% 
+  ungroup %>% 
+  transmute(AGENCY_NAME, 
+            FOCUS,
+            TYPE = TYPE_PATTERN,
+            ACRONYM)
+
 # WRITE DATA ----
 
-write_csv(agencies, here::here("wa-state-agencies/data/wa-state-agencies.csv"))
+write_csv(agencies_types, here::here("wa-state-agencies/data/wa-state-agencies.csv"))
