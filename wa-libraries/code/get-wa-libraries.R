c3c992b7ad00a13344350bb8d7bb7a1f0b1d162c
Tiernan Martin <tiernan.martin@gmail.com>
2018-05-29 17:30:43 -0700

    add public libraries in Washington State

---
+++ wa-libraries/code/get-wa-libraries.R
@@ -0,0 +1,62 @@
+# SETUP ----
+
+library(janitor) 
+library(glue)
+library(rvest)
+library(placement) 
+library(sf)
+library(tidyverse)
+
+
+# GET DATA ----
+
+libraries_table_url <- "http://www.publiclibraries.com/washington.htm"
+
+libs <- read_html(libraries_table_url) %>% 
+  html_nodes(css = "table") %>% 
+  html_table(header = TRUE) %>% 
+  flatten_df() %>% 
+  rename_all(snakecase::to_screaming_snake_case)
+
+# GECODE DATA ----
+
+libs_addr <- libs %>%   
+  mutate(ADDRESS_FULL = as.character(glue("{ADDRESS}, {CITY}, WA, {ZIP}")))
+
+geocode_fun <- function(address){
+    geocode_url(address, 
+                auth="standard_api", 
+                privkey="",  # this can be replaced with a free private key from Google's API
+                clean=TRUE, 
+                add_date='today', 
+                verbose=TRUE) 
+  }
+
+libs_sf <- libs_addr %>%  
+  mutate(geocode_addr =  map(ADDRESS_FULL, geocode_fun)) %>% 
+  unnest %>% 
+  janitor::clean_names("screaming_snake") %>% 
+  st_as_sf(coords = c("LNG", "LAT")) %>% 
+  st_set_crs(4326) 
+
+get_coord <- function(x, coord){x %>% st_coordinates() %>% as.data.frame() %>% pluck(coord)}
+
+libs_ready <- libs_sf %>% 
+  transmute(LIBRARY_NAME = LIBRARY,
+            FORMATTED_ADDRESS,
+            PHONE = PHONE) %>% 
+  separate(FORMATTED_ADDRESS, c("ADDRESS","CITY","STATE_ZIP","COUNTRY"), sep = ", ") %>%  
+  separate(STATE_ZIP, c("STATE", "ZIP_CODE"), sep = " ") %>% 
+  transmute(LIBRARY_NAME,
+         PHONE,
+         ADDRESS,
+         CITY,
+         STATE,
+         ZIP_CODE,
+         LNG = map_dbl(geometry,get_coord,"X"),
+         LAT = map_dbl(geometry,get_coord,"Y"))
+
+
+# WRITE DATA ----
+
+st_write(libs_ready, here::here("wa-libraries/data/wa-libraries.gpkg"))
---
+++ wa-libraries/code/wa-libraries-data-dictionary.R
@@ -0,0 +1,34 @@
+# SETUP ----
+ 
+library(snakecase)
+library(datapasta)
+library(sf)
+library(tidyverse)
+
+# GET DATA ----
+
+vars <- st_read(here::here("wa-libraries/data/wa-libraries.gpkg")) %>% 
+  colnames() %>%  
+  to_mixed_case %>% 
+  str_replace_all("_"," ") %>% 
+  {tibble(VARIABLE = .)}
+
+# dpasta(tibble(VARIABLE = vars))
+
+
+data_dictionary <- tibble::tribble(
+        ~VARIABLE,                                          ~DESCRIPTION,                                                  ~NOTE,
+   "Library Name",                             "The name of the library",                                                     NA,
+          "Phone",                                    "The phone number",                                                     NA,
+        "Address",                                  "The street address",  "Formatted according to the Google Maps API standard",
+           "City",               "The city where the library is located",                                                     NA,
+          "State",              "The state where the library is located",                                                     NA,
+       "Zip Code",            "The zip code where the libary is located",                                                     NA,
+            "Lng",  "The longitude of the libraries geographic location",                                                     NA,
+            "Lat",   "The latitude of the libraries geographic location",                                                     NA,
+           "geom",              "The geometry of the libraries location",                                 "Geometry type: POINT"
+  )
+
+# WRITE DATA DICTIONARY ----
+
+write_csv(data_dictionary, here::here("wa-libraries/data/wa-libraries-data-dictionary.csv"))
